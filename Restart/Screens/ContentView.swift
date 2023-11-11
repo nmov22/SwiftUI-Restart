000fd7739bffff05be12263a7d63ca4c212a7d58
Noel Velasco <nvelasco@yondu.com>
2023-11-11 11:36:12 +0800

    - initial commit

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Noel Velasco on 2/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+
+
+  var body: some View {
+    ZStack {
+      if isOnboardingViewActive {
+        OnboardingView()
+      } else {
+        HomeView()
+      }
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Restart/Screens/OnboardingView.swift
@@ -0,0 +1,183 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by Noel Velasco on 2/16/22.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+
+  @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+  @State private var buttonOffSet: CGFloat = 0
+  @State private var isAnimating: Bool = false
+  @State private var imageOffSet: CGSize = .zero
+  @State private var indicatorOpacity: Double = 1.0
+  @State private var textTitle: String = "Share."
+
+  let hapticFeedback = UINotificationFeedbackGenerator()
+
+  var body: some View {
+    ZStack {
+      Color("ColorBlue")
+        .ignoresSafeArea(.all, edges: .all)
+
+      VStack(spacing: 20) {
+        //MARK: - Header
+        Spacer()
+
+        VStack(spacing: 0) {
+          Text(textTitle)
+            .font(.system(size:60))
+            .fontWeight(.heavy)
+            .foregroundColor(.white)
+            .transition(.opacity)
+            .id(textTitle)
+
+          Text("""
+          It's not how much we give but how much we love we put into giving.
+          """)
+            .font(.title)
+            .fontWeight(.light)
+            .foregroundColor(.white)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal, 10)
+        }//: Header
+        .opacity(isAnimating ? 1: 0)
+        .offset(y: isAnimating ? 0: -40)
+        .animation(.easeOut(duration: 1), value: isAnimating)
+
+        //MARK: - Center
+
+        ZStack {
+          CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+            .offset(x: imageOffSet.width * -1)
+            .blur(radius: abs(imageOffSet.width / 5))
+            .animation(.easeOut(duration: 1), value: imageOffSet)
+
+          Image("character-1")
+            .resizable()
+            .scaledToFit()
+            .opacity(isAnimating ? 1 : 0)
+            .animation(.easeOut(duration: 1), value: isAnimating)
+            .offset(x: imageOffSet.width * 1.2, y: 0)
+            .rotationEffect(.degrees(Double(imageOffSet.width / 20)))
+            .gesture(
+              DragGesture()
+                .onChanged { gesture in
+                  if abs(imageOffSet.width) <= 150 {
+                    imageOffSet = gesture.translation
+                  }
+
+                  withAnimation(.linear(duration: 0.25)) {
+                    indicatorOpacity = 0
+                    textTitle = "Give."
+                  }
+                }
+                .onEnded { _ in
+                  imageOffSet = .zero
+
+                  withAnimation(.linear(duration: 0.25)) {
+                    indicatorOpacity = 1.0
+                    textTitle = "Share."
+                  }
+                }
+            )
+            .animation(.easeOut(duration: 1.25), value: imageOffSet)
+        }//: Center
+        .overlay(
+          Image(systemName: "arrow.left.and.right.circle")
+            .font(.system(size: 44, weight: .ultraLight))
+            .foregroundColor(.white)
+            .offset(y: 20)
+            .opacity(isAnimating ? 1 : 0)
+            .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+            .opacity(indicatorOpacity)
+          , alignment: .bottom
+        )
+
+        Spacer()
+        //MARK: - Footer
+
+        ZStack {
+          // Parts of the custom Button
+
+          // 1. Background (Static)
+          Capsule()
+            .fill(Color.white.opacity(0.2))
+
+          Capsule()
+            .fill(Color.white.opacity(0.2))
+            .padding(8)
+          // 2. Call-to-action (Static)
+          Text("Get Started")
+            .font(.system(.title3, design: .rounded))
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .offset(x: 20)
+          // 3. Capsule (Dynamic Width)
+          HStack {
+            Capsule()
+              .fill(Color("ColorRed"))
+              .frame(width: buttonOffSet + 80)
+
+            Spacer()
+          }
+          // 4. Circle (Draggable)
+          HStack {
+            ZStack{
+              Circle()
+                .fill(Color("ColorRed"))
+              Circle()
+                .fill(.black.opacity(0.15))
+                .padding(8)
+              Image(systemName: "chevron.right.2")
+                .font(.system(size: 24, weight: .bold))
+            }.foregroundColor(.white)
+              .frame(width: 80, height: 80, alignment: .center)
+              .offset(x: buttonOffSet)
+              .gesture(
+                DragGesture()
+                  .onChanged { gesture in
+                    if (gesture.translation.width > 0 && buttonOffSet <= buttonWidth - 80) {
+                      buttonOffSet = gesture.translation.width
+                    }
+                  }
+                  .onEnded { _ in
+                    withAnimation(Animation.easeOut(duration: 0.4)) {
+                      if buttonOffSet > buttonWidth / 2 {
+                        hapticFeedback.notificationOccurred(.success)
+                        playSound(sound: "chimeup", type: "mp3")
+                        buttonOffSet = buttonWidth - 80
+                        isOnboardingViewActive = false
+                      } else {
+                        hapticFeedback.notificationOccurred(.warning)
+                        buttonOffSet = 0
+                      }
+                    }
+                  }
+              )
+            Spacer()
+          } //: HStack
+        }//: Footer
+        .frame(width: buttonWidth, height: 80, alignment: .center)
+        .padding()
+        .opacity(isAnimating ? 1 : 0)
+        .offset(y: isAnimating ? 0 : 40)
+        .animation(.easeOut(duration: 1), value: isAnimating)
+      } //: VStack
+    } //: ZStack
+    .onAppear(perform: {
+      isAnimating = true
+    })
+    .preferredColorScheme(.dark)
+  }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+  static var previews: some View {
+    OnboardingView()
+  }
+}
